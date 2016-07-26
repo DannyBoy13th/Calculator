46424fe966068c297cac5bea58d4b1d1aab8f48b
Daniel Solo <danielsolo13th@gmail.com>
2016-07-26 11:34:50 +0300

    more tests

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -14,7 +14,50 @@ public class CalculatorTest {
         provider.initialize();
         calculator.setOperations(provider.getAllOperations());
         calculator.setParser(parser);
-        System.out.println(calculator.calculate(a));
+        System.out.println("Result: " + a + "=" + calculator.calculate(a));
 
     }
+
+    @Test
+    public void testCalculate1() throws Exception {
+        Provider provider = new Provider();
+        ParserImpl parser = new ParserImpl();
+        Calculator calculator = new Calculator();
+        double result = 0;
+        String a = "13+45";
+        provider.initialize();
+        calculator.setOperations(provider.getAllOperations());
+        calculator.setParser(parser);
+        System.out.println("Result: " + a + "=" + calculator.calculate(a));
+
+    }
+
+    @Test
+    public void testCalculate3() throws Exception {
+        Provider provider = new Provider();
+        ParserImpl parser = new ParserImpl();
+        Calculator calculator = new Calculator();
+        double result = 0;
+        String a = "85 + 95 ";
+        provider.initialize();
+        calculator.setOperations(provider.getAllOperations());
+        calculator.setParser(parser);
+        System.out.println("Result: " + a + "=" + calculator.calculate(a));
+
+    }
+
+    @Test
+    public void testCalculate4() throws Exception {
+        Provider provider = new Provider();
+        ParserImpl parser = new ParserImpl();
+        Calculator calculator = new Calculator();
+        double result = 0;
+        String a = "1843    -    1283 ";
+        provider.initialize();
+        calculator.setOperations(provider.getAllOperations());
+        calculator.setParser(parser);
+        System.out.println("Result: " + a + "=" + calculator.calculate(a));
+
+    }
+
 }
--- src/test/java/ParserImplTest.java
+++ src/test/java/ParserImplTest.java
@@ -7,8 +7,10 @@ import java.util.List;
 public class ParserImplTest {
 
     ParserImpl parsedExpression = new ParserImpl();
+
     @org.junit.Test
-    public void testParsering() throws Exception {
+
+    public void testParser() throws Exception {
         String example = "31+5";
         parsedExpression.parse(example);
         List<BigDecimal> operands = parsedExpression.getOperands();
@@ -17,8 +19,21 @@ public class ParserImplTest {
             System.out.println("operand " + operand);
         }
         System.out.println("operator " + operator);
-
-
     }
+
+    @org.junit.Test
+
+    public void testParser1() throws Exception {
+        String example = "450  -  352";
+        parsedExpression.parse(example);
+        List<BigDecimal> operands = parsedExpression.getOperands();
+        char operator = parsedExpression.getOperator();
+        for (BigDecimal operand : operands) {
+            System.out.println("operand " + operand);
+        }
+        System.out.println("operator " + operator);
+    }
+
+
 
 }
